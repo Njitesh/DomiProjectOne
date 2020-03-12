d14e97cf9f88b748b6c4f8b5b81381995fc85331
Njitesh <jitesh.nayak123@gmail.com>
2020-03-12 09:46:05 +0530

    Sample project to fetch data from web services and display in the list

---
+++ app/src/main/java/com/sample/demo/FeedItemHolder.java
@@ -0,0 +1,22 @@
+package com.sample.demo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.partyplanner.jsonfeed.R;
+
+public class FeedItemHolder extends RecyclerView.ViewHolder {
+    TextView tittle;
+    TextView description;
+    NetworkImageView feedImage;
+
+    public FeedItemHolder(View view) {
+        super(view);
+        tittle = (TextView) view.findViewById(R.id.feed_title);
+        description = (TextView) view.findViewById(R.id.feedDescription);
+        feedImage = (NetworkImageView) view.findViewById(R.id.icon);
+    }
+
+}
